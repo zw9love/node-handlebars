93e93df598be7ea9baa995b86565ed3f741901de
zw9love <18514075699@163.com>
2018-12-25 11:06:33 +0800

    basely finish

--- app.js
+++ app.js
@@ -4,29 +4,49 @@
  **/
 var path = require('path');
 var http = require('http');
+var https = require('https');
 var express = require('express');
 var fs = require("fs");
 var exphbs  = require('express-handlebars');
 var app = express();
 app.use('/static', express.static('static'));//静态文件托管
-app.engine('handlebars', exphbs());//npm 安装的mustache没有提供模板引擎，不注册模板引擎会报错Error: Module "mustache" does not provide a view engine.
-//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+//app.engine('handlebars', exphbs());//npm 安装的mustache没有提供模板引擎，不注册模板引擎会报错Error: Module "mustache" does not provide a view engine.
+//app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: "views/layouts/"}));
+app.engine('handlebars', exphbs({defaultLayout: 'index', layoutsDir: "templates/", partialsDir: 'templates/'}));
 app.set('views', path.join(__dirname, '/templates'));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, response) {
-    let data = {
-        title: '首页111212',
-        name: 'zengwei',
-        age: 28,
-        hasList: true,
-        list: [
-            {name: '大熊', age: 30, list: ['a', 'b']} ,
-            {name: '静香', age: 30, list: ['c', 'd']} ,
-            {name: '胖虎', age: 30, list: ['e', 'f']} ,
-        ],
-    }
-    response.render('index', data);//把读取的数据填充进模板
+    https.get('https://api.pgwxd.com/api/BrandCover', function (res) {
+        // res.setEncoding('utf8');
+        var rawData = '';
+        res.on('data', function (chunk) {
+            rawData += chunk;
+        });
+        res.on('end', function () {
+            try {
+                const parsedData = JSON.parse(rawData);
+                //console.log(parsedData);
+                let data = {
+                    title: '首页11',
+                    name: 'zengwei',
+                    age: 28,
+                    hasList: true,
+                    list: [
+                        {name: '大熊', age: 30, list: ['a', 'b']} ,
+                        {name: '静香', age: 30, list: ['c', 'd']} ,
+                        {name: '胖虎', age: 30, list: ['e', 'f']} ,
+                    ],
+                    apiData: parsedData
+                }
+                response.render('index', data);//把读取的数据填充进模板
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    });
+    //response.render('index', data);//把读取的数据填充进模板
+
 });
 
 app.get('/show/:ids', function(req, response) {
